57c0a461cd5377f2a39fcdf0c3bd01d6232c4868
Ben Burwood <ben.burwood@outlook.com>
2024-05-08 20:53:27 +0100

    Add Icon and Head Title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "nuxt-icon"],
+  head: {
+    title: "Ben Burwood",
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/bb.ico' }
+    ]
+  }
 })
