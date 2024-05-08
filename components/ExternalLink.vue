23fdad829901aff306c1f2bb50bb8dd0d1f04991
Ben Burwood <ben.burwood@outlook.com>
2024-05-08 19:17:08 +0100

    Add External Link Component w/ Iconify

---
+++ components/ExternalLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <a :href="link">
+        <Icon :name="icon" color="black" />
+    </a>
+</template>
+
+<script setup>
+const props = defineProps({
+    icon: String,
+    link: String
+});
+</script>
+
+<style scoped>
+.icon {
+    width: 40px;
+    height: 40px;
+    transition: transform 0.3s ease-in-out;
+}
+
+.icon:hover {
+  transform: scale(1.1);
+}
+</style>
