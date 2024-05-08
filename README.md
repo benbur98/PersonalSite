c3f4a52c449fe87a703db2d8d342bbf37e1e383b
Ben Burwood <ben.burwood@outlook.com>
2024-05-08 18:28:40 +0100

    Add Light/Dark Mode Toggle

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # PersonalSite
+
+## Stack
+
+Nuxt3 (Vue3 Composition) with NuxtUI (HeadlessUI, Tailwind CSS, HeroIcons)
+
+Jetbrains Mono Font - https://www.jetbrains.com/lp/mono/
---
+++ components/LightDarkModeToggle.vue
@@ -0,0 +1,30 @@
+<template>
+    <UButton
+        class="light-dark-toggle"
+        :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+        size="md"
+        color="gray"
+        variant="ghost"
+        @click="isDark = !isDark" />
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+const isDark = computed({
+    get() {
+        return colorMode.value === 'dark';
+    },
+    set() {
+        colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+    }
+});
+</script>
+
+<style scoped>
+.light-dark-toggle {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    padding: 1rem;
+}
+</style>
