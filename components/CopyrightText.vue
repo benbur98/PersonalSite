7931d3a831d4b61d9c87a10dc4543e6357c83eb5
Ben Burwood <ben.burwood@outlook.com>
2024-05-08 20:00:09 +0100

    Add Copyright Text Container

---
+++ components/CopyrightText.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="copyright-container">
+        <p class="copyright-text">&copy; {{ year }} | {{ owner }}</p>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    year: String, owner: String
+});
+</script>
+
+<style scoped>
+.copyright-text {
+    color: #636362;
+    font-size: 10px;
+}
+
+.copyright-container {
+    width: fit-content;
+    bottom: 0;
+    padding: 10px;
+    background-color: #edf2f7;
+    border-top-left-radius: 12px;
+    border-top-right-radius: 12px;
+}
+</style>
--- components/ExternalLink.vue
+++ components/ExternalLink.vue
@@ -1,6 +1,6 @@
 <template>
     <a :href="link">
-        <Icon :name="icon" color="black" />
+        <Icon :name="icon" color="#2e2e2e" size="3em" />
     </a>
 </template>
 
@@ -13,8 +13,6 @@ const props = defineProps({
 
 <style scoped>
 .icon {
-    width: 40px;
-    height: 40px;
     transition: transform 0.3s ease-in-out;
 }
 
