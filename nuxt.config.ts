670dd7e542651d48026e2bb38f3e17be646527fa
Ben Burwood <ben.burwood@outlook.com>
2024-06-05 14:18:45 +0100

    Add .nojekyll File

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "nuxt-icon"],
-  target: "static",
-  ssr: false,
-  nitro: {
-    prerender: {
-      routes: ['/404.html']
-    }
-  },
   app: {
     head: {
       title: "Ben Burwood",
@@ -15,5 +8,12 @@ export default defineNuxtConfig({
         { rel: 'icon', type: 'image/x-icon', href: '/bb.ico' }
       ]
     }
-  }
+  },
+  // target: "static",
+  // ssr: false,
+  // nitro: {
+  //   prerender: {
+  //     routes: ['/404.html']
+  //   }
+  // },
 })
